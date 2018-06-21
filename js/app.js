34d9b9b9e14bf0788da42b8cbf2e95674d7ef99f
Ben Harris <ben@harrishills.com>
2018-06-21 00:23:23 -0700

    Adding randomizer functionality to BusMall app

--- js/app.js
+++ js/app.js
@@ -17,80 +17,106 @@
 // 3. Create an EventListener to capture the user's input (25 rounds), and store their votes for later display.
 // 4. Create a list (for now) to display the user's voting record after the user votes 25 times.
 
+
+// A. Refactor to use object constructor
+// B. Setup myGlobals to provide on-going reference to image objects
+
 // ------------------------------------------------------------------------------------------------------------
 // SETUP
 // ------------------------------------------------------------------------------------------------------------
 // Safety Goggles ON!
 'use strict';
 
+// Global Variables
+var myGlobals = {
+  imgDisplay: 0,
+  myImages: [],
+  imgQueue: [],
+  // imgSelection: [],
+};
+
 // ------------------------------------------------------------------------------------------------------------
 // OBJECTS
 // ------------------------------------------------------------------------------------------------------------
-// Assets
-var assets = {
-  myImg: [0,1,2],
-  imgSet: [
-    'bag.jpg',
-    'banana.jpg',
-    'bathroom.jpg',
-    'boots.jpg',
-    'breakfast.jpg',
-    'bubblegum.jpg',
-    'chair.jpg',
-    'cthulhu.jpg',
-    'dog-duck.jpg',
-    'dragon.jpg',
-    'pen.jpg',
-    'pet-sweep.jpg',
-    'scissors.jpg',
-    'shark.jpg',
-    'sweep.jpg',
-    'tauntaun.jpg',
-    'unicorn.jpg',
-    'usb.gif',
-    'water-can.jpg',
-    'wine-glass.jpg'
-  ],
-  imgQueue: [],
-};
+// Object constructor for new images
+function myImg (filename, friendlyname) {
+  this.filename = filename;
+  this.friendlyname = friendlyname;
+
+  // Store a reference to each new instance in a global 'myImages' array
+  myGlobals.myImages.push(this);
+}
 
 // ------------------------------------------------------------------------------------------------------------
 // METHODS
 // ------------------------------------------------------------------------------------------------------------
 
-
 // ------------------------------------------------------------------------------------------------------------
 // OPERATIONS
 // ------------------------------------------------------------------------------------------------------------
-// Randomize photos on button click -- send myImg[n] through randomizer (foreach)
+// Create image objects
+new myImg ('bag.jpg',         'R2D2 Suitcase');                 // eslint-disable-line
+new myImg ('banana.jpg',      'Banana Slicer');                 // eslint-disable-line
+new myImg ('bathroom.jpg',    'Bathroom Tablet Stand');         // eslint-disable-line
+new myImg ('boots.jpg',       'Open-Toe Rain boots');           // eslint-disable-line
+new myImg ('breakfast.jpg',   'All-in-One Breakfast Maker');    // eslint-disable-line
+new myImg ('bubblegum.jpg',   'Meatball Flavored Gum');         // eslint-disable-line
+new myImg ('chair.jpg',       'Rounded Chair');                 // eslint-disable-line
+new myImg ('cthulhu.jpg',     'Cthulhu Action Figure');         // eslint-disable-line
+new myImg ('dog-duck.jpg',    'Dog-to-Duck Converter');         // eslint-disable-line
+new myImg ('dragon.jpg',      'Dragon Meat');                   // eslint-disable-line
+new myImg ('pen.jpg',         'Silverware Pens');               // eslint-disable-line
+new myImg ('pet-sweep.jpg',   'Pet Sweep');                     // eslint-disable-line
+new myImg ('scissors.jpg',    'Pizza Scissors');                // eslint-disable-line
+new myImg ('shark.jpg',       'Shark Sleeping Bag');            // eslint-disable-line
+new myImg ('sweep.png',       'Baby Broom Outfit');             // eslint-disable-line
+new myImg ('tauntaun.jpg',    'Taun-Taun Sleeping Bag');        // eslint-disable-line
+new myImg ('unicorn.jpg',     'Unicorn Meat');                  // eslint-disable-line
+new myImg ('usb.gif',         'Tentacle USB Drive');            // eslint-disable-line
+new myImg ('water-can.jpg',   'Self Watering Can');             // eslint-disable-line
+new myImg ('wine-glass.jpg',  'Death Star Wine Glass');         // eslint-disable-line
 
-imageRender(assets.myImg[0],assets.myImg[1],assets.myImg[2]);
+// Set the total number of images to be displayed
+myGlobals.imgDisplay = 3;
+
+// Render the initial image selection
+for (var imgCount = 0; imgCount < myGlobals.imgDisplay; imgCount++) {
+  var imgSelection = randomImg();
+  imageRender(imgSelection);
+}
+
+// Vote event Handler
+
+// Restore images from imgQueue array into myImages array
+while(myGlobals.imgQueue.length > 0) {
+  var restoreImg = myGlobals.imgQueue.pop(myGlobals.imgQueue[0]);
+  myGlobals.myImages.push(restoreImg);  
+}
 
 // ------------------------------------------------------------------------------------------------------------
 // FUNCTIONS
 // ------------------------------------------------------------------------------------------------------------
-function imageRender(num1, num2, num3) {
-  var imgTarget1 = document.getElementById('img1');
-  var imgTarget2 = document.getElementById('img2');
-  var imgTarget3 = document.getElementById('img3');
+function imageRender(imgIndex) {
+  var imgTarget = document.getElementById('imgGen');
 
-  var img1 = document.createElement('img');
-  img1.src = 'img/' + assets.imgSet[num1];
-  imgTarget1.appendChild(img1);
+  // Create <li> element
+  var liEl = document.createElement('li');
+  var imgEl = document.createElement('img');
+  imgEl.src = 'img/' + myGlobals.myImages[imgIndex].filename;
+  liEl.appendChild(imgEl);
+  imgTarget.appendChild(liEl);
 
-  var img2 = document.createElement('img');
-  img2.src = 'img/' + assets.imgSet[num2];
-  imgTarget2.appendChild(img2);
+  // Add current image object to imgQueue array
+  // console.log(myGlobals.myImages[imgIndex]);
+  myGlobals.imgQueue.push(myGlobals.myImages[imgIndex]);
 
-  var img3 = document.createElement('img');
-  img3.src = 'img/' + assets.imgSet[num3];
-  imgTarget3.appendChild(img3);
-
-  // Push used images into assets.imgQueue array for temporary holding
+  // Remove current image object from myImages array
+  myGlobals.myImages.splice(imgIndex, 1);
 }
 
-function randomNum(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max-min)) + min;
+function randomImg() {
+  var randomIndex = myGlobals.myImages[Math.floor(Math.random() * myGlobals.myImages.length)];
+  randomIndex = myGlobals.myImages.indexOf(randomIndex);
+  // console.log(randomIndex);
+  return randomIndex;
 }
