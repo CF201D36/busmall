1111dc1506281f7376f70324d03843461f8f4a40
Ben Harris <ben@harrishills.com>
2018-06-24 19:37:37 -0700

    Commitinng nearly working randomizer code to busmall-start branch for grading

--- js/app.js
+++ js/app.js
@@ -8,20 +8,6 @@
 // Tags:          Code Fellows, 201, Practice, Lab, Website
 
 // ------------------------------------------------------------------------------------------------------------
-// PROJECT GOALS
-// ------------------------------------------------------------------------------------------------------------
-// -- MONDAY REQUIREMENTS --
-// 1. Create a photo object
-// 2. Create a random photo selection function/method that chooses 3 random images to display and temporarily
-//    stores them in an array so that they are prevented from being displayed during the next set.
-// 3. Create an EventListener to capture the user's input (25 rounds), and store their votes for later display.
-// 4. Create a list (for now) to display the user's voting record after the user votes 25 times.
-
-
-// A. Refactor to use object constructor
-// B. Setup myGlobals to provide on-going reference to image objects
-
-// ------------------------------------------------------------------------------------------------------------
 // SETUP
 // ------------------------------------------------------------------------------------------------------------
 // Safety Goggles ON!
@@ -29,6 +15,9 @@
 
 // Global Variables
 var myGlobals = {
+  imgReel: document.getElementById('genImgReel'),
+  displayCount: document.getElementById('displayCount'),
+  voteCount: 25,
   imgDisplay: 0,
   myImages: [],
   imgQueue: [],
@@ -39,9 +28,11 @@ var myGlobals = {
 // OBJECTS
 // ------------------------------------------------------------------------------------------------------------
 // Object constructor for new images
-function myImg (filename, friendlyname) {
+function myImg (filename, friendlyname, votesReceived, timesDisplayed) {
   this.filename = filename;
   this.friendlyname = friendlyname;
+  this.votesReceived = votesReceived;
+  this.timesDisplayed = timesDisplayed;
 
   // Store a reference to each new instance in a global 'myImages' array
   myGlobals.myImages.push(this);
@@ -55,63 +46,72 @@ function myImg (filename, friendlyname) {
 // OPERATIONS
 // ------------------------------------------------------------------------------------------------------------
 // Create image objects
-new myImg ('bag.jpg',         'R2D2 Suitcase');                 // eslint-disable-line
-new myImg ('banana.jpg',      'Banana Slicer');                 // eslint-disable-line
-new myImg ('bathroom.jpg',    'Bathroom Tablet Stand');         // eslint-disable-line
-new myImg ('boots.jpg',       'Open-Toe Rain boots');           // eslint-disable-line
-new myImg ('breakfast.jpg',   'All-in-One Breakfast Maker');    // eslint-disable-line
-new myImg ('bubblegum.jpg',   'Meatball Flavored Gum');         // eslint-disable-line
-new myImg ('chair.jpg',       'Rounded Chair');                 // eslint-disable-line
-new myImg ('cthulhu.jpg',     'Cthulhu Action Figure');         // eslint-disable-line
-new myImg ('dog-duck.jpg',    'Dog-to-Duck Converter');         // eslint-disable-line
-new myImg ('dragon.jpg',      'Dragon Meat');                   // eslint-disable-line
-new myImg ('pen.jpg',         'Silverware Pens');               // eslint-disable-line
-new myImg ('pet-sweep.jpg',   'Pet Sweep');                     // eslint-disable-line
-new myImg ('scissors.jpg',    'Pizza Scissors');                // eslint-disable-line
-new myImg ('shark.jpg',       'Shark Sleeping Bag');            // eslint-disable-line
-new myImg ('sweep.png',       'Baby Broom Outfit');             // eslint-disable-line
-new myImg ('tauntaun.jpg',    'Taun-Taun Sleeping Bag');        // eslint-disable-line
-new myImg ('unicorn.jpg',     'Unicorn Meat');                  // eslint-disable-line
-new myImg ('usb.gif',         'Tentacle USB Drive');            // eslint-disable-line
-new myImg ('water-can.jpg',   'Self Watering Can');             // eslint-disable-line
-new myImg ('wine-glass.jpg',  'Death Star Wine Glass');         // eslint-disable-line
+new myImg ('bag.jpg',         'R2D2 Suitcase',                  0,0);   // eslint-disable-line
+new myImg ('banana.jpg',      'Banana Slicer',                  0,0);   // eslint-disable-line
+new myImg ('bathroom.jpg',    'Bathroom Tablet Stand',          0,0);   // eslint-disable-line
+new myImg ('boots.jpg',       'Open-Toe Rain boots',            0,0);   // eslint-disable-line
+new myImg ('breakfast.jpg',   'All-in-One Breakfast Maker',     0,0);   // eslint-disable-line
+new myImg ('bubblegum.jpg',   'Meatball Flavored Gum',          0,0);   // eslint-disable-line
+new myImg ('chair.jpg',       'Rounded Chair',                  0,0);   // eslint-disable-line
+new myImg ('cthulhu.jpg',     'Cthulhu Action Figure',          0,0);   // eslint-disable-line
+new myImg ('dog-duck.jpg',    'Dog-to-Duck Converter',          0,0);   // eslint-disable-line
+new myImg ('dragon.jpg',      'Dragon Meat',                    0,0);   // eslint-disable-line
+new myImg ('pen.jpg',         'Silverware Pens',                0,0);   // eslint-disable-line
+new myImg ('pet-sweep.jpg',   'Pet Sweep',                      0,0);   // eslint-disable-line
+new myImg ('scissors.jpg',    'Pizza Scissors',                 0,0);   // eslint-disable-line
+new myImg ('shark.jpg',       'Shark Sleeping Bag',             0,0);   // eslint-disable-line
+new myImg ('sweep.png',       'Baby Broom Outfit',              0,0);   // eslint-disable-line
+new myImg ('tauntaun.jpg',    'Taun-Taun Sleeping Bag',         0,0);   // eslint-disable-line
+new myImg ('unicorn.jpg',     'Unicorn Meat',                   0,0);   // eslint-disable-line
+new myImg ('usb.gif',         'Tentacle USB Drive',             0,0);   // eslint-disable-line
+new myImg ('water-can.jpg',   'Self Watering Can',              0,0);   // eslint-disable-line
+new myImg ('wine-glass.jpg',  'Death Star Wine Glass',          0,0);   // eslint-disable-line
 
 // Set the total number of images to be displayed
 myGlobals.imgDisplay = 3;
 
-// Render the initial image selection
-for (var imgCount = 0; imgCount < myGlobals.imgDisplay; imgCount++) {
-  var imgSelection = randomImg();
-  imageRender(imgSelection);
-}
+// Define and render the initial image selection
+imageGenerator();
 
-// Vote event Handler
+
+// Store user votes to localStorage
 
 // Restore images from imgQueue array into myImages array
-while(myGlobals.imgQueue.length > 0) {
-  var restoreImg = myGlobals.imgQueue.pop(myGlobals.imgQueue[0]);
-  myGlobals.myImages.push(restoreImg);  
-}
+reloadImgSet();
+
+// Render List
+
+// Render Chart
+
+// Add to localStorage
+
+// Event Listeners
+myGlobals.displayCount.addEventListener('click', updateDisplay); // Update display count
+myGlobals.imgReel.addEventListener('click', imageRefresher); // Refresh image selection
+myGlobals.imgReel.addEventListener('click', imageRefresher); // Refresh image selection
+
 
 // ------------------------------------------------------------------------------------------------------------
 // FUNCTIONS
 // ------------------------------------------------------------------------------------------------------------
-function imageRender(imgIndex) {
-  var imgTarget = document.getElementById('imgGen');
+function updateDisplay() {
+  myGlobals.imgDisplay = myGlobals.displayCount.Value;
+  clearNode('imgGen');
+  imageGenerator();
+}
 
-  // Create <li> element
-  var liEl = document.createElement('li');
-  var imgEl = document.createElement('img');
-  imgEl.src = 'img/' + myGlobals.myImages[imgIndex].filename;
-  liEl.appendChild(imgEl);
-  imgTarget.appendChild(liEl);
+function imageRefresher() {
+  
+  clearNode('imgGen');
+  imageGenerator();
+}
 
-  // Add current image object to imgQueue array
-  // console.log(myGlobals.myImages[imgIndex]);
-  myGlobals.imgQueue.push(myGlobals.myImages[imgIndex]);
-
-  // Remove current image object from myImages array
-  myGlobals.myImages.splice(imgIndex, 1);
+function imageGenerator() {
+  for (var imgCount = 0; imgCount < myGlobals.imgDisplay; imgCount++) {
+    console.log(imgCount);
+    var imgSelection = randomImg();
+    imageRender(imgSelection);
+  }
 }
 
 function randomImg() {
@@ -119,4 +119,55 @@ function randomImg() {
   randomIndex = myGlobals.myImages.indexOf(randomIndex);
   // console.log(randomIndex);
   return randomIndex;
+}
+
+function imageRender(imgIndex) {
+  var imgTarget = document.getElementById('imgGen');
+
+  // Create <li> element
+  var liEl = document.createElement('li');
+  var imgEl = document.createElement('img');
+
+  // Add content
+  imgEl.src = 'img/' + myGlobals.myImages[imgIndex].filename;
+  imgEl.name = imgIndex;
+  imgEl.id = 'img'+imgIndex;
+  console.log(imgEl);
+
+  // Append elements
+  liEl.appendChild(imgEl);
+  imgTarget.appendChild(liEl);
+
+  // Add current image object to imgQueue array
+  console.log(myGlobals.myImages[imgIndex]);
+  myGlobals.imgQueue.push(myGlobals.myImages[imgIndex]);
+
+  // Remove current image object from myImages array
+  myGlobals.myImages.splice(imgIndex, 1);
+}
+
+
+
+
+function clearNode(myId) {
+  var node = document.getElementById(myId);
+  while (node.hasChildNodes()) {
+    node.removeChild(node.firstChild);
+  }
+}
+
+function voteCounter () {
+  if (myGlobals.voteCount > 0) {
+    myGlobals.voteCount--;
+    var counter = document.getElementById('myCount');
+    counter.textContent = myGlobals.voteCount;
+  }
+}
+
+function reloadImgSet () {
+  console.log('Funciton ran!');
+  while(myGlobals.imgQueue.length > 0) {
+    var restoreImg = myGlobals.imgQueue.pop(myGlobals.imgQueue[0]);
+    myGlobals.myImages.push(restoreImg); 
+  }
 }
